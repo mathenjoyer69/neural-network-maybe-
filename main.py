2fd4512b6c54feb2a343ab5a79384dd3829de3ae
mathenjoyer69 <159634821+mathenjoyer69@users.noreply.github.com>
2025-03-28 12:08:51 +0300

    Update main.py
    add the points that the neural network will try to fit into

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import random
+import matplotlib.pyplot
+from numpy.ma.core import count
+
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
@@ -8,3 +11,12 @@ x_i = 0
 R = 0.0014
 
 v = [10*random.random()-5 for i in range(13)]
+
+def f(x):
+    return x**2
+
+x_interval = 1.5
+x_points = 24
+n_i = np.linspace(-x_interval, 1.5, x_points)
+N = [(float(i),float(f(i))) for i in n_i]
+L = len(N)
